4b877059505f9f530dc5a15cd91382d927b82fd8
Abhi <007abhinavchaudhary@gmail.com>
2024-07-19 21:17:50 +0530

    Day2

---
+++ src/findAllDuplicates.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class findAllDuplicates {
+    public static List<Integer> findDuplicates(int[] arr) {
+        int i=0;
+        List<Integer> res=new ArrayList<>();
+        while(i<arr.length){
+            int curr=arr[i]-1;
+            if(arr[i]!=arr[curr]){
+                int temp=arr[i];
+                arr[i]=arr[curr];
+                arr[curr]=temp;
+            }
+
+            else{
+                i++;
+            }}
+        for(int j=0;j<arr.length;j++){
+            if(arr[j]!=j+1){
+                res.add(arr[j]);
+            }
+        }
+
+        return res;
+
+    }
+
+    public static void main(String[] args) {
+        int arr[]={4,3,2,7,8,2,3,1};
+        System.out.println(findDuplicates(arr));
+    }
+}
