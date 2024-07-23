b6ec16b08b8fb4b1c606b07f3ee5d15b441fcb9d
Abhi <007abhinavchaudhary@gmail.com>
2024-07-23 23:50:12 +0530

    Day 6

--- src/Subarraysumk.java
+++ src/Subarraysumk.java
@@ -4,6 +4,7 @@ public class Subarraysumk {
     public static  int subarraySum(int[] nums, int k) {
         HashMap<Integer,Integer> map=new HashMap<>();
         map.put(0,1);
+        int ans=0;
         int sum=0;
         int count=0;
         for(int i=0;i<nums.length;i++){
