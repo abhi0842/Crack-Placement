0d2a190d3432b9467a1ac2b0e86dc5db9014144e
Abhi <007abhinavchaudhary@gmail.com>
2024-08-03 17:08:59 +0530

    Day 16

---
+++ src/BalancedBinaryTree.java
@@ -0,0 +1,32 @@
+public class BalancedBinaryTree {
+    public class TreeNode {
+        int val;
+       TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    private int height(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        return 1 + Math.max(height(node.left), height(node.right));
+    }
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+
+        int leftHeight = height(root.left);
+        int rightHeight = height(root.right);
+        if (Math.abs(leftHeight - rightHeight) > 1) {
+            return false;
+        }
+        return isBalanced(root.left) && isBalanced(root.right);
+    }
+}
